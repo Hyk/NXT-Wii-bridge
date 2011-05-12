15262853eaf6a4e41fd9e716a0aa3fce5cb52779
Rasmus Gulbaek <rasmus.gulbaek@gmail.com>
2011-05-12 17:59:12 +0200

    Created project

---
+++ NXT_Wii_bridge/NXT_Wii_bridge/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  NXT_Wii_bridge
+//
+//  Created by Rasmus Gulbaek on 12/05/11.
+//  Copyright 2011 Gulbaek I/S. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
